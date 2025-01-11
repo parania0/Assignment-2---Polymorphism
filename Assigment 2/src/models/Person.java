1f63736561401992937566b16693fece55363900
erkeg <erkegalielbosynovic@gmail.com>
2025-01-11 21:17:32 +0500

    init project

--- Assigment 2/src/models/Person.java
+++ Assigment 2/src/models/Person.java
@@ -1,13 +1,16 @@
 package models;
 
-public class Person implements Payable,Comparable<Person>{
-    private static int idCounter = 1;
+public class Person implements Payable, Comparable<Person> {
+
+    private static int idCounter = 1; //na nahui
     private int id = idCounter++;
+
     private String name;
     private String surname;
 
     public Person() {
     }
+
     public Person(String name, String surname) {
         this.name = name;
         this.surname = surname;
@@ -17,6 +20,12 @@ public class Person implements Payable,Comparable<Person>{
     public int compareTo(Person o) {
         return Double.compare(this.getPaymentAmount(), o.getPaymentAmount());
     }
+
+    @Override
+    public double getPaymentAmount() {
+        return 0;
+    }
+
     @Override
     public String toString() {
         return id + ". " + name + " " + surname;
@@ -45,6 +54,5 @@ public class Person implements Payable,Comparable<Person>{
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
 
 }
